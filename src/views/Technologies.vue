039671d8a6129164ece6807144c2fb5502622f6c
GonzaOrtiz <gonzaortiz1506@gmail.com>
2021-11-06 15:50:01 -0300

    technology view

--- src/views/Technologies.vue
+++ src/views/Technologies.vue
@@ -4,41 +4,42 @@
     <div class="container">
       <h3 class="text-center mb-3">Tecnologías</h3>
       <hr>
-      <div v-if="!tableView" class="d-flex justify-content-around">
+      <div v-if="tableView" class="d-flex justify-content-around">
         <div class="card col-5 text-center bg-light align-items-center shadow-lg">
           <h4 class="m-3">Ingrese el nombre de una Tecnología</h4>
-          <input class="mb-3"  placeholder="Ejemplo: Javascript">
-          <button class="btn btn-primary btn-lg mb-3" @click="createTechnology" >Crear</button>
-        </div>
+          <input v-model="technology.name" class="mb-3"  placeholder="Ejemplo: Javascript">
+          <div class="row">
+            <div class="col-sm-12 text-center">
+            <button class="btn btn-primary btn-lg mb-2" @click="createTechnology" >Crear</button>&nbsp;
+            <button @click="btnRegresar" class="btn btn-danger btn-lg mb-2" >Regresar</button>
+            </div>
+          </div>
+          </div>
       </div>
 
-      <div v-if="tableView" class="d-flex justify-content-around">
+      <div v-if="!tableView" class="d-flex justify-content-around">
         <div class="card col-5 text-center bg-light align-items-center shadow-lg">
           <table class="table">
             <thead>
             <tr>
-              <th scope="col">#</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th scope="col">Handle</th>
+              <th scope="col">Nombre</th>
+              <th scope="col">Editar/Eliminar/Detalle</th>
             </tr>
             </thead>
             <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
+            <tr v-for="element in technologiesArray" :key="element.id">
+              <td>{{ element.name }}</td>
+              <td><a href="">Editar</a><a href="">Eliminar</a><a href="">Detalle</a></td>
             </tr>
             </tbody>
           </table>
-          <button @click="btnRegresar" class="btn btn-danger btn-lg mb-3" >Regresar</button>
+          <button @click="btnIrANuevo" class="btn btn-success btn-lg mb-3" >Nueva Tecnología</button>
         </div>
       </div>
 
-      <div class="text-center mt-5">
-        <button v-if="!tableView" class="btn btn-success btn-lg font-weight-bold  shadow-lg" @click="viewTechnologies" >Ver Tecnologías</button>
-      </div>
+<!--      <div class="text-center mt-5">-->
+<!--        <button v-if="!tableView" class="btn btn-success btn-lg font-weight-bold  shadow-lg" @click="viewTechnologies" >Ver Tecnologías</button>-->
+<!--      </div>-->
     </div>
     </body>
   </div>
@@ -53,23 +54,33 @@ export default {
       tableView: false,
       technology:{
         name: null
-      }
+      },
+      technologiesArray: null,
     };
   },
   methods:{
     createTechnology(){
-      axios.post("http://localhost:3000/technology", this.technology).then((result) => {
+      axios.post("http://localhost:3000/technologies", this.technology).then((result) => {
         console.log(result);
       });
       alert('Creado correctamente');
     },
-    viewTechnologies(){
-      this.tableView = true;
-    },
+    pullTechnologies(){
+      axios.get("http://localhost:3000/technologies").then((result) => {
+        this.technologiesArray = result.data;
+        console.log(result);
+      });
+},
     btnRegresar(){
       this.tableView = false;
+    },
+    btnIrANuevo(){
+      this.tableView = true;
     }
 
+  },
+  created(){
+    this.pullTechnologies();
   }
 }
 </script>
